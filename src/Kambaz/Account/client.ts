d9600a9b19cbc96972f3bd99cac855931f3b3b7b
ZY-Garrete <63596544+ZY-Garrete@users.noreply.github.com>
2025-04-13 21:29:38 -0400

    Deploy frontend to Netlify from branch a6

--- src/Kambaz/Account/client.ts
+++ src/Kambaz/Account/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosWithCredentials = axios.create({ withCredentials: true });
-export const REMOTE_SERVER = process.env.VITE_REMOTE_SERVER || process.env.REACT_APP_REMOTE_SERVER;
+export const REMOTE_SERVER = process.env.VITE_REMOTE_SERVER_A6 || process.env.REACT_APP_REMOTE_SERVER;
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
 
 export const signin = async (credentials: any) => {
--- src/Kambaz/Courses/client.ts
+++ src/Kambaz/Courses/client.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const REMOTE_SERVER = process.env.VITE_REMOTE_SERVER || process.env.REACT_APP_REMOTE_SERVER;
+export const REMOTE_SERVER = process.env.VITE_REMOTE_SERVER_A6 || process.env.REACT_APP_REMOTE_SERVER;
 const COURSES_API = `${REMOTE_SERVER}/api/courses`;
 export const createModuleForCourse = async (courseId: string, module: any) => {
     const response = await axios.post(
